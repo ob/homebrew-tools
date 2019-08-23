4ec2f3972ee765d7e5724edc7e8a9f6964039c48
Oscar Bonilla <6f6231@gmail.com>
2019-08-22 20:57:35 -0700

    Update for latest

--- Casks/swift-utils.rb
+++ Casks/swift-utils.rb
@@ -1,11 +1,12 @@
 cask 'swift-utils' do
-    version '0.0.0'
-    sha256 '0eefe1d339f89e514091e15cb2eb6cba2997858b57a44ed38cf5adfd6f704d60'
-    url 'https://github.com/ob/swift-prebuilt/releases/download/v0.0.0/tools-macosx-x86_64.tar.gz'
+    version 'swift-5.0.1-RELEASE-20190823.1'
+    sha256 '8df29282a3c4f7b10269d3e680784aa1373673ec621b0b0ab9f031ec993d420d'
+    url 'https://github.com/ob/swift-prebuilt/releases/download/swift-5.0.1-RELEASE-20190823.1/tools-macosx-x86_64.tar.gz'
     name 'swift-utils'
     homepage 'https://github.com/ob/swift-prebuilt/blob/master/README.md'
     appcast 'https://github.com/ob/swift-prebuilt/releases.atom'
 
     binary 'tools/bin/swift-ide-test'
     binary 'tools/bin/c-index-test'
+    binary 'tools/bin/FileCheck'
 end

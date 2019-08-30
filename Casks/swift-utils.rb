c2c92ee1d6ef9515406ebf5213346460ef6cdd7e
Oscar Bonilla <6f6231@gmail.com>
2019-08-30 09:07:15 -0700

    Update to latest (makes c-index-test work)

--- Casks/swift-utils.rb
+++ Casks/swift-utils.rb
@@ -1,7 +1,7 @@
 cask 'swift-utils' do
-    version 'swift-5.0.1-RELEASE-20190823.1'
-    sha256 '8df29282a3c4f7b10269d3e680784aa1373673ec621b0b0ab9f031ec993d420d'
-    url 'https://github.com/ob/swift-prebuilt/releases/download/swift-5.0.1-RELEASE-20190823.1/tools-macosx-x86_64.tar.gz'
+    version 'swift-5.0.1-RELEASE-20190827.1'
+    sha256 '553f264f40e2ee0a29327cff7eb111045ded5f2704d99749de5794c2f6496112'
+    url 'https://github.com/ob/swift-prebuilt/releases/download/swift-5.0.1-RELEASE-20190827.1/tools-macosx-x86_64.tar.gz'
     name 'swift-utils'
     homepage 'https://github.com/ob/swift-prebuilt/blob/master/README.md'
     appcast 'https://github.com/ob/swift-prebuilt/releases.atom'
@@ -9,4 +9,6 @@ cask 'swift-utils' do
     binary 'tools/bin/swift-ide-test'
     binary 'tools/bin/c-index-test'
     binary 'tools/bin/FileCheck'
+    artifact 'tools/lib/libclang.dylib', target: '/usr/local/lib/libclang.dylib'
+    artifact 'tools/lib/libIndexStore.dylib', target: '/usr/local/lib/libIndexStore.dylib'
 end
